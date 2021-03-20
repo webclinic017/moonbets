5bccf98c29e1d87fef447d99b77ec18374e3714e
mandosss <mandosss91@gmail.com>
2021-03-20 04:26:32 -0500

    updated constants, added core functions for script

--- src/backend/constants.py
+++ src/backend/constants.py
@@ -18,19 +18,14 @@ SEC_URL = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK={}&owner=exclude'
 
 
 # File names for stats
-CASHFLOW_YEAR = 'cashflow_Y_{}'
-CASHFLOW_QUATER = 'cashflow_Q_{}'
-INCOMESTATEMENT_YEAR = 'incomestatement_Y_{}'
-INCOMESTATEMENT_QUARTER = 'incomestatement_Q_{}'
-BALANCESHEET_YEAR = 'blanacesheet_Y_{}'
-BALANCESHEET_QUATER = 'balancesheet_Q_{}'
-RATIOS_YEAR = 'ratios_Y_{}'
-RATIOS_QUATER = 'ratios_Q_{}'
-KEYMETRICS_YEAR = 'keymetrics_Y_{}'
-KEYMETRICS_QUATER = 'keymetrics_Q_{}'
+CASHFLOW = 'cashflow_{}'
+INCOMESTATEMENT = 'incomestatement_{}'
+BALANCESHEET = 'balanacesheet_{}'
+RATIOS = 'ratios_{}'
+KEYMETRICS = 'keymetrics_{}'
 COMPANYPROFILE = 'companyprofile_{}'
-INSIDERTRADING = 'insider_{}'
-SEC = 'SEC_{}'
+INSIDERTRADING = 'insider'
+SEC = 'SEC'
 
 # File Names for users
 WATCH_LIST = 'Watch_Lists'
--- src/backend/external_endpoints.py
+++ src/backend/external_endpoints.py
@@ -7,71 +7,108 @@ from src.backend.constants import SEC_URL, APIKEY
 
 #   Date should be in YYYY:MM:DD
 #   only three months maximum if range is given
+
+
 def get_earning_calendar(from_date: str = None, to_date: str = None):
     try:
-        raw_data = api.earning_calendar(APIKEY, from_date=from_date, to_date=to_date)
+        raw_data = api.earning_calendar(APIKEY,
+                                        from_date=from_date,
+                                        to_date=to_date)
         return raw_data
     except:
         print("no data found")
 
 
-def get_cash_flow_statment(ticker, period='annual'):
+def get_cash_flow_statment(ticker: str, period: str = 'annual'):
     try:
-        raw_data = api.cash_flow_statement(apikey=APIKEY, symbol=ticker, period=period)
+        default_limit = 60
+        if period is 'annual':
+            default_limit = 15
+        raw_data = api.cash_flow_statement(apikey=APIKEY,
+                                           symbol=ticker,
+                                           period=period,
+                                           limit=default_limit)
         return raw_data
     except:
         print("no data found")
 
 
-def get_income_statment(ticker, period='annual'):
+def get_income_statment(ticker: str, period: str = 'annual'):
     try:
-        raw_data = api.income_statement(apikey=APIKEY, symbol=ticker, period=period)
+        default_limit = 60
+        if period is 'annual':
+            default_limit = 15
+        raw_data = api.income_statement(apikey=APIKEY,
+                                        symbol=ticker,
+                                        period=period,
+                                        limit=default_limit)
         return raw_data
     except:
         print("no data found")
 
 
-def get_balance_sheet_statement(ticker, period='annual'):
+def get_balance_sheet_statement(ticker: str, period: str = 'annual'):
     try:
-        raw_data = api.balance_sheet_statement(apikey=APIKEY, symbol=ticker, period=period)
+        default_limit = 60
+        if period is 'annual':
+            default_limit = 15
+        raw_data = api.balance_sheet_statement(apikey=APIKEY,
+                                               symbol=ticker,
+                                               period=period,
+                                               limit=default_limit)
         return raw_data
     except:
         print("no data found")
 
 
-def get_ratios(ticker, period='annual'):
+def get_ratios(ticker: str, period: str = 'annual'):
     try:
-        raw_data = api.financial_ratios(apikey=APIKEY, symbol=ticker, period=period)
+        default_limit = 60
+        if period is 'annual':
+            default_limit = 15
+        raw_data = api.financial_ratios(apikey=APIKEY,
+                                        symbol=ticker,
+                                        period=period,
+                                        limit=default_limit)
         return raw_data
     except:
         print("no data found")
 
 
-def get_key_metrics(ticker, period='annual'):
+def get_key_metrics(ticker: str, period: str = 'annual'):
     try:
-        raw_data = api.key_metrics(apikey=APIKEY, symbol=ticker, period=period)
+        default_limit = 60
+        if period is 'annual':
+            default_limit = 15
+        raw_data = api.key_metrics(apikey=APIKEY,
+                                   symbol=ticker,
+                                   period=period,
+                                   limit=default_limit)
         return raw_data
     except:
         print("no data found")
 
 
-def get_company_profile(ticker):
+def get_company_profile(ticker: str):
     try:
-        raw_data = api.company_profile(apikey=APIKEY, symbol=ticker)
+        raw_data = api.company_profile(apikey=APIKEY,
+                                       symbol=ticker)
         return raw_data
     except:
         print("no data found")
 
 
-def get_insider_trading(ticker):
+def get_insider_trading(ticker: str):
     try:
-        raw_data = api.insider_trading(apikey=APIKEY, symbol=ticker)
+        raw_data = api.insider_trading(apikey=APIKEY,
+                                       symbol=ticker,
+                                       limit=500)
         return raw_data
     except:
         print("no data found")
 
 
-def sec_statements(ticker):
+def sec_statements(ticker: str):
     try:
         raw_data = requests.get(SEC_URL.format(ticker))
         return raw_data.url
